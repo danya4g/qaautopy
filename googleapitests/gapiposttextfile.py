9af663f6e2db18ef40dcdc989f67e19aad8b893a
Daniil Shelepin <@shelepin.daniil>
2025-09-26 07:17:58 +0300

    add some tests

---
+++ googleapitests/gapiposttextfile.py
@@ -0,0 +1,80 @@
+import requests
+
+class Test_New_Location():
+    """Создаем новую локацию"""
+
+    def test_create_new_location(self):
+        """Создание новой локации"""
+        base_url = "https://rahulshettyacademy.com"  # Базовая url
+        key = "?key=qaclick123"  # Обязательный параметр для всех запросов
+        post_resource = "/maps/api/place/add/json"  # Ресурс метода Post
+
+        post_url = base_url + post_resource + key
+        print(post_url)
+
+        json_for_create_new_location = {
+            "location": {
+                "lat": -38.383494,
+                "lng": 33.427362
+            },
+            "accuracy": 50,
+            "name": "Frontline house",
+            "phone_number": "(+91) 983 893 3937",
+            "address": "29, side layout, cohen 09",
+            "types": [
+                "shoe park",
+                "shop"
+            ],
+            "website": "http://google.com",
+            "language": "French-IN"
+        }
+
+        result_post = requests.post(post_url, json=json_for_create_new_location)
+        result_post.raise_for_status()
+        print(result_post.text)
+        assert 200 == result_post.status_code
+        print("Успешно создана новая локация!")
+        
+        """Создаем текстовый файл, в который отправим значения метода POST"""
+        post_data = result_post.json()
+        filename = 'post_response_ids.txt'
+        with open (filename, 'w', encoding='utf-8') as file:
+            for i in range(5):
+                result_post = requests.post(post_url, json=json_for_create_new_location)
+                result_post.raise_for_status()
+                file.write(f'{post_data['place_id']}\n')
+
+        check_post = result_post.json()
+        check_info_post = check_post.get("status")
+        print(f'Статус код ответа: {check_info_post}')
+        assert check_info_post == "OK"
+        print("Статус код корректен")
+        place_id = check_post.get("place_id")
+        print(f'Place_id: {place_id}')
+      
+        """Получение локаций"""
+        get_resource = "/maps/api/place/get/json"
+        get_url = base_url + get_resource + key + "&place_id=" + place_id
+        print(get_url)
+        result_get = requests.get(get_url)
+        result_get.raise_for_status()
+        print(result_get.text)
+        print(f'Статус код: {result_get.status_code}')
+        assert 200 == result_get.status_code
+        print("Проверка создания локации прошла успешно!")
+
+run_test = Test_New_Location()
+run_test.test_create_new_location()
+        #  """Тестирование того, что локация изменилась"""
+        # result_get = requests.get(get_url)
+        # print(result_get.json())
+
+        # check_response_get = result_get.json()
+        # print(f'Статус код: {result_get.status_code}')
+        # assert result_get.status_code == 200, 'Ошибка, статус код не совпадает'
+        # print("Статус код GET корректен")
+
+        # actual_address = check_response_get.get('address')
+        # print(actual_address)
+        # assert actual_address == new_address, 'Ошибка, адрес не изменился'
+        # print("Адрес изменен")
